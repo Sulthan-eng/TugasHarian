8507e6dce6cd6e4c6b7a03cef613d7699a392908
Sulthan-eng <farizanfawwaz54@gmail.com>
2025-03-28 15:02:26 +0800

    membuat class VolumeTabungOOP.java dan menyelesaikan seluruh kode

---
+++ src/Modul1/VolumeTabungOOP.java
@@ -0,0 +1,24 @@
+package Modul1;
+
+public class VolumeTabungOOP {
+    public static void main(String[] args) {
+        Volume tabung = new Volume(12, 14);
+        double Volume = tabung.hitungVolume();
+        System.out.println("Volume Tabung : " + Volume);
+    }
+}
+
+class Volume{
+    double PI = Math.PI;
+    double tinggi;
+    double jarijari;
+
+    public Volume(double tinggi, double jarijari) {
+        this.tinggi = tinggi;
+        this.jarijari = jarijari;
+    }
+
+    double hitungVolume(){
+        return PI*jarijari*jarijari*tinggi;
+    }
+}
