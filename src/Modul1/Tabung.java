179cf30e85890d24577b35f12dadbf0cb1524c50
Sulthan-eng <farizanfawwaz54@gmail.com>
2025-03-28 15:01:30 +0800

    memperbaiki kode

--- src/Modul1/Tabung.java
+++ src/Modul1/Tabung.java
@@ -2,13 +2,9 @@ package Modul1;
 
 public class Tabung {
     public static void main(String[] args) {
-        LuasIsi tabung1 = new LuasIsi(10, 7);
-        double LuasIsi = tabung1.hitungLuas();
+        LuasIsi tabung = new LuasIsi(10, 7);
+        double LuasIsi = tabung.hitungLuas();
         System.out.println("Luas Isi Tabung : " + LuasIsi);
-
-        Volume tabung2 = new Volume(12, 7);
-        double Volume = tabung2.hitungVolume();
-        System.out.println("Volume Tabung : " + Volume);
     }
 }
 class LuasIsi {
@@ -25,19 +21,4 @@ class LuasIsi {
         return 2*PI*jarijari*(jarijari+tinggi);
     }
 
-}
-
-class Volume{
-    double PI = Math.PI;
-    double tinggi;
-    double jarijari;
-
-    public Volume(double tinggi, double jarijari) {
-        this.tinggi = tinggi;
-        this.jarijari = jarijari;
-    }
-
-    double hitungVolume(){
-        return PI*jarijari*jarijari*tinggi;
-    }
 }
