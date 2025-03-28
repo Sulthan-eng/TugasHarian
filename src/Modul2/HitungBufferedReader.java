e65b777b4b293cbe33a9acd4ad7fdf050b37ad4b
Sulthan-eng <farizanfawwaz54@gmail.com>
2025-03-28 15:58:40 +0800

    membuat program menghitung keliling lingkaran

--- src/Modul2/HitungBufferedReader.java
+++ src/Modul2/HitungBufferedReader.java
@@ -1,4 +1,27 @@
 package Modul2;
+import java.io.*;
 
+class HitungKelilingLingkaranBuffered {
+    double jarijari;
+    double PI = Math.PI;
+
+    public HitungKelilingLingkaranBuffered(double jarijari) {
+        this.jarijari = jarijari;
+    }
+    double hitungKeliling(){
+        return 2 * PI * jarijari;
+    }
+}
 public class HitungBufferedReader {
+    public static void main(String[] args) throws IOException {   // throws IOException digunakan untuk mendeklarasikan bahwa suatu metode bisa melempar IOException, sehingga pemanggil kode harus meneruskan exception
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Menghitung Keliling Lingkaran");
+        System.out.println("Masukkan jari-jari : ");
+        double jarijari = Double.parseDouble(br.readLine()); //mengambil input user, mengonversi ke tipe data double, lalu menyimpan ke variabel jarijari
+
+        HitungKelilingLingkaranBuffered lingkaran = new HitungKelilingLingkaranBuffered(jarijari);
+        double keliling = lingkaran.hitungKeliling();
+        System.out.println("Keliling Lingkaran : " + keliling);
+    }
 }
