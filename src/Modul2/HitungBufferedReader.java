d2d71b319b5917140f9a16d13fdd2bb313621494
Sulthan-eng <farizanfawwaz54@gmail.com>
2025-03-28 18:16:56 +0800

    menambah program menghitung luas prisma segitiga

--- src/Modul2/HitungBufferedReader.java
+++ src/Modul2/HitungBufferedReader.java
@@ -2,26 +2,93 @@ package Modul2;
 import java.io.*;
 
 class HitungKelilingLingkaranBuffered {
-    double jarijari;
+    double jariJari;
     double PI = Math.PI;
 
-    public HitungKelilingLingkaranBuffered(double jarijari) {
-        this.jarijari = jarijari;
+    public HitungKelilingLingkaranBuffered(double jariJari) {
+        this.jariJari = jariJari;
     }
     double hitungKeliling(){
-        return 2 * PI * jarijari;
+        return 2 * PI * jariJari;
     }
 }
+
+class HitungLuasTrapesiumBuffered {
+    double sisiAtas;
+    double sisiBawah;
+    double tinggi;
+
+    public HitungLuasTrapesiumBuffered(double sisiAtas, double sisiBawah, double tinggi) {
+        this.sisiAtas = sisiAtas;
+        this.sisiBawah = sisiBawah;
+        this.tinggi = tinggi;
+    }
+
+    public double hitungLuas() {
+        return 0.5 * (sisiAtas + sisiBawah) * tinggi;
+    }
+}
+class HitungLuasPrismaSegitigaBuffered {
+    double alas;
+    double tinggiSegitiga ;
+    double tinggiPrisma;
+
+    public HitungLuasPrismaSegitigaBuffered(double alas, double tinggiSegitiga, double tinggiPrisma) {
+        this.alas = alas;
+        this.tinggiSegitiga = tinggiSegitiga;
+        this.tinggiPrisma = tinggiPrisma;
+    }
+    private double hitungLuasAlas(){
+        return 0.5 * alas * tinggiSegitiga;
+    }
+    private double kelilingAlas(){
+        double sisiMiring = Math.sqrt((alas * alas) + (tinggiSegitiga * tinggiSegitiga));
+        return alas + tinggiSegitiga + sisiMiring;
+    }
+    private double luasSelimut(){
+        return kelilingAlas() * tinggiPrisma;
+    }
+    public double hitungLuasPrisma(){
+        double luasAlas = hitungLuasAlas();
+        double luasSelimut = luasSelimut();
+        return (2 * luasAlas) + luasSelimut;
+    }
+}
+
 public class HitungBufferedReader {
     public static void main(String[] args) throws IOException {   // throws IOException digunakan untuk mendeklarasikan bahwa suatu metode bisa melempar IOException, sehingga pemanggil kode harus meneruskan exception
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("Menghitung Keliling Lingkaran");
         System.out.println("Masukkan jari-jari : ");
-        double jarijari = Double.parseDouble(br.readLine()); //mengambil input user, mengonversi ke tipe data double, lalu menyimpan ke variabel jarijari
+        double jariJari = Double.parseDouble(br.readLine()); //mengambil input user, mengonversi ke tipe data double, lalu menyimpan ke variabel jarijari
 
-        HitungKelilingLingkaranBuffered lingkaran = new HitungKelilingLingkaranBuffered(jarijari);
+        HitungKelilingLingkaranBuffered lingkaran = new HitungKelilingLingkaranBuffered(jariJari); //buat objek "lingkaran" dari class HitungKelilingLingkaranBuffered dan memanggil konstruktor yang menginisialisasi variabel jarijari
         double keliling = lingkaran.hitungKeliling();
         System.out.println("Keliling Lingkaran : " + keliling);
+
+        System.out.println("Menghitung Luas Trapesium : ");
+        System.out.println("Masukkan sisi atas : ");
+        double sisiAtas = Double.parseDouble(br.readLine());
+        System.out.println("Masukkan sisi bawah : ");
+        double sisiBawah = Double.parseDouble(br.readLine());
+        System.out.println("Masukkan tinggi : ");
+        double tinggi = Double.parseDouble(br.readLine());
+
+        HitungLuasTrapesiumBuffered trapesium = new HitungLuasTrapesiumBuffered(sisiAtas, sisiBawah, tinggi);
+        double luas = trapesium.hitungLuas();
+        System.out.println("Luas Trapesium : " + luas);
+
+        System.out.println("Menghitung Luas Prisma Segitiga : ");
+        System.out.println("Masukkan alas : ");
+        double alas = Double.parseDouble(br.readLine());
+        System.out.println("Masukkan tinggi segitia : ");
+        double tinggiSegitiga = Double.parseDouble(br.readLine());
+        System.out.println("Masukkan tinggi prisma : ");
+        double tinggiPrisma = Double.parseDouble(br.readLine());
+
+        HitungLuasPrismaSegitigaBuffered prismaSegitiga = new HitungLuasPrismaSegitigaBuffered(alas, tinggiSegitiga, tinggiPrisma);
+        double luasPrisma = prismaSegitiga.hitungLuasPrisma();
+        System.out.println("Luas Prisma Segitiga : " + luasPrisma);
     }
 }
